a974887aca2a1544b1def0aa64fbb177ddc26738
Zainan Victor Zhou <zzn@google.com>
2018-08-26 12:51:45 -0700

    Fix test failure by removing broken deploy command

--- migrations/2_migrate_contracts.js
+++ migrations/2_migrate_contracts.js
@@ -8,18 +8,18 @@ module.exports = async function(deployer) {
 
   //deployer.deploy(SimplestVote1202);
 
-  const devAccounts =       [
-    0xd73A01C4b9D7175EFa05f414E757e75fc1e14b9F,
-    0x168fbF3566166A088ca6D392F00087197DccBD02,
-    0xA791c85dF0CC0866dddDF5dCfC2dda639dFA83Bf
-  ];
-
-  await deployer.deploy(BasicErc20Token);
-  // await deployer.deploy(TokenVote1202);
-  // tokenVote1202 = await TokenVote1202.deployed();
-  // await tokenVote1202.init(SampleToken.address, [1, 2, 3], devAccounts);
-
-  await deployer.deploy(AdvancedTokenVote1202);
-  advancedTokenVote1202 = await AdvancedTokenVote1202.deployed();
-  await advancedTokenVote1202.createIssue(BasicErc20Token.address, [1, 2, 3], devAccounts, 'Issue 0: should we issue more tokens?');
+  // const devAccounts =       [
+  //   0xd73A01C4b9D7175EFa05f414E757e75fc1e14b9F,
+  //   0x168fbF3566166A088ca6D392F00087197DccBD02,
+  //   0xA791c85dF0CC0866dddDF5dCfC2dda639dFA83Bf
+  // ];
+  //
+  // await deployer.deploy(BasicErc20Token);
+  // // await deployer.deploy(TokenVote1202);
+  // // tokenVote1202 = await TokenVote1202.deployed();
+  // // await tokenVote1202.init(SampleToken.address, [1, 2, 3], devAccounts);
+  //
+  // await deployer.deploy(AdvancedTokenVote1202);
+  // advancedTokenVote1202 = await AdvancedTokenVote1202.deployed();
+  // await advancedTokenVote1202.createIssue(BasicErc20Token.address, [1, 2, 3], devAccounts, 'Issue 0: should we issue more tokens?');
 };
--- truffle.js
+++ truffle.js
@@ -12,12 +12,6 @@
  *   },
  */
 
-const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
-const privateKey = process.env.PRIVATE_KEY;
-console.assert(privateKey, 'Please set private key by `export PRIVATE_KEY=<your private key>`');
-const infuraApikey = process.env.INFURA_APIKEY;
-console.assert(infuraApikey, 'Please set private key by `export INFURA_APIKEY=<your infura API key>`');
-
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -32,7 +26,14 @@ module.exports = {
       gasPrice: 2e5,
     },
     ropsten: {
-      provider: new HDWalletProvider(privateKey, "https://ropsten.infura.io/"+infuraApikey),
+      provider: function () {
+        const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+        const privateKey = process.env.PRIVATE_KEY;
+        console.assert(privateKey, 'Please set private key by `export PRIVATE_KEY=<your private key>`');
+        const infuraApikey = process.env.INFURA_APIKEY;
+        console.assert(infuraApikey, 'Please set private key by `export INFURA_APIKEY=<your infura API key>`');
+        return new HDWalletProvider(privateKey, "https://ropsten.infura.io/"+infuraApikey);
+      },
       network_id: 3,
       gas: 4e6,
       gasPrice: 2e10,
