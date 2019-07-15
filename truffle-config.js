895e9805656d76226fcad05fc8ab80d2020fc628
Zainan Victor Zhou <zzn@zzn.im>
2019-07-14 17:43:17 -0700

    Fix tests

--- truffle-config.js
+++ truffle-config.js
@@ -20,10 +20,10 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
-    dev: {
+    development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*", // Match any network id
+      network_id: "5777", // Match any network id
       gas: 4e6,
       gasPrice: 2e5,
     },
@@ -41,4 +41,24 @@ module.exports = {
       gasPrice: 2e10,
     }
   },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
 };
