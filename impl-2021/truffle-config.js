54a38f94f9aee173cce4de8820f250f4229a19a6
Zainan Victor Zhou <zzn+github@zzn.im>
2021-11-01 20:22:33 -0700

    update truffle network development with new port and network id

--- impl-2021/truffle-config.js
+++ impl-2021/truffle-config.js
@@ -44,7 +44,7 @@ module.exports = {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     network_id: "5777",
     },
     // Another network with more advanced options...
     // advanced: {
